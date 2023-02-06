934cb34cbffee162daf6ce542a19ba9ef2f823f1
ZingerEngineer <ramyisez@gmail.com>
2023-02-06 15:27:13 +0200

    Changed localhost db connection to 127001

--- db/connect.ts
+++ db/connect.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 export const connect = async () => {
   try {
     mongoose.set('strictQuery', false)
-    await mongoose.connect('mongodb://localhost/dashboard-db')
+    await mongoose.connect('mongodb://127.0.0.1/dashboard-db')
     console.log('Connected to db')
   } catch (error) {
     console.log('Error happened', error)
